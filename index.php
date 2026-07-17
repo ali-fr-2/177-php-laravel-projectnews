2fe4840cfcc97eaafec6d02233b081d83c8b13cd
ali-fr-2 <alifarjamy2@gmail.com>
2026-07-17 22:42:41 +0330

    178-session-start

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+session_start();
+
+define("BASE_PATH", __DIR__);
+
+define("ERROR", true);
+
+// define("DOMAIN",currentpath(),"projectnews");
+
+define("HOST", "localhost");
+define("USERNAME", "root");
+define("PASSWORD", "");
+define("DB_NAME", "projectnews");
