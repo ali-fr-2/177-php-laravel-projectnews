02bdc79fb3d2af810f9c674097bed08e88b48911
ali-fr-2 <alifarjamy2@gmail.com>
2026-07-18 19:45:02 +0330

    180-helper-in-php

--- index.php
+++ index.php
@@ -12,3 +12,19 @@ define("HOST", "localhost");
 define("USERNAME", "root");
 define("PASSWORD", "");
 define("DB_NAME", "projectnews");
+ 
+
+// function redirect($url){
+//     header("location:".$url);
+//     exit();
+// }
+// redirect('main.php');
+
+define("BASE_URL","http://localhost/177-projectnews/");
+
+function redirect($url){
+    header("location:".trim(BASE_URL,"/ ")."/".trim($url,"/ "));
+    exit();
+}
+redirect('main.php');
+
